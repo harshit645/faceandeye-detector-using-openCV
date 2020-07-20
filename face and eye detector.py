d6b8527058604e3ca3c8a10db86412b5965bdd63
harshit645 <harshitsinghal641@gmail.com>
2020-07-20 13:29:43 +0530

    face and eye detector

--- face and eye detector.py
+++ face and eye detector.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 face_features=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eyes_features=cv2.CascadeClassifier("harrcascase_eye.xml")
@@ -12,7 +13,9 @@ a=0
 while True:
     a=a+1
     check,frame=video.read()
+    #check is a boolean variable if returns true when camera reads the image
     print(check)
+    #frame represent the first image that video Video Capture
     print(frame)
 
     #if we want to convert our frame into gray frames
@@ -23,6 +26,7 @@ while True:
     face_coordinates=face_features.detectMultiScale(gray_frame,scaleFactor=1.05,
     minNeighbors=5)
 
+    #to make rectngle over the face using face_features
     for x,y,w,h in face_coordinates:
         frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
         eyes_color=frame[y:y+h,x:x+w]
@@ -30,20 +34,27 @@ while True:
 
         #we usually use gray_eyes for determining coordinates of eyes
         eyes_coordinates=eyes_features.detectMultiScale(eyes_gray)
+
+        #to make rectngle over the eyes using eyes_features
         for (ex,ey,ew,eh) in eyes_coordinates:
             eyes_color=cv2.rectangle(eyes_color,(ex,ey),(ex+ew,ey+eh),
             (255,0,0),2)
 
 
+    #to show the image
     cv2.imshow("capturing",frame)
 
+    #wait for 1milisecond
     key=cv2.waitKey(1)
 
     if key==ord('q'):
         break
 
+#no. of frames
 print(a)
 
+#to release the video
 video.release()
 
+#closing all open windows
 cv2.destroyAllWindows()
